7a31d252f162db96e50ec262b74e56b8b35604b5
yurilozorio <yurilozorio@gmail.com>
2019-07-24 19:10:58 -0300

    Updated Url

--- index.js
+++ index.js
@@ -1,17 +1,149 @@
-const tronwallet = require('./tronwallet')
+const SubClient = require('subscriptions-transport-ws')
+const ApClient = require('apollo-client')
+const apolloLinkWs = require('apollo-link-ws')
+const apolloCache = require('apollo-cache-inmemory')
+const ws = require('ws')
+const gql = require('graphql-tag')
 
-tronwallet.blockListener((data) => {
-  console.log(data)
-})
 
-tronwallet.transactionListener((data) => {
-  console.log(data)
-})
+const GRAPHQL_ENDPOINT = 'tcp://data.tronwallet.me/graphql';
 
-tronwallet.contractEventListener('',(data) => {
-  console.log(data)
-})
+const client = new SubClient.SubscriptionClient(GRAPHQL_ENDPOINT, {
+  reconnect: true,
+}, ws);
 
-tronwallet.contractLogListener("TEEXEWrkMFKapSMJ6mErg39ELFKDqEs6w3", (data) => {
-  console.log(data)
-})
+const wsLink = new apolloLinkWs.WebSocketLink(client);
+
+const apolloClient = new ApClient.ApolloClient({
+    link: wsLink,
+    cache: new apolloCache.InMemoryCache(),
+});
+
+const blockListener = (handler) => {
+  apolloClient.subscribe({
+    query: gql`
+      subscription block{
+        eventsTopicBlock{
+          timeStamp
+          triggerName
+          blockNumber
+          blockHash
+          transactionSize
+          latestSolidifiedBlockNumber
+          transactionList
+        }
+      }`,
+    variables: {}
+  }).subscribe({
+    next (data) {
+      handler(data)
+    }
+  });
+}
+
+const transactionListener = (handler) => {
+  apolloClient.subscribe({
+    query: gql`
+      subscription transaction{
+        eventsTopicTransaction{
+          timeStamp
+          triggerName
+          transactionId
+          blockHash
+          blockNumber
+          energyUsage
+          energyFee
+          originEnergyUsage
+          energyUsageTotal
+          netUsage
+          netFee
+          result
+          contractAddress
+          contractType
+          feeLimit
+          contractCallValue
+          contractResult
+          fromAddress
+          toAddress
+          assetName
+          assetAmount
+          latestSolidifiedBlockNumber
+          internalTrananctionList
+        }
+      }`,
+    variables: {}
+  }).subscribe({
+    next (data) {
+      handler(data)
+    }
+  });
+}
+
+const contractEventListener = (contractAddress = '', handler) => {
+  apolloClient.subscribe({
+    query: gql`
+      subscription contractEvent($contractAddress: String){
+        eventsTopicContractEvent(contractAddress: $contractAddress){
+          timeStamp
+          triggerName
+          uniqueId
+          transactionId
+          contractAddress
+          callerAddress
+          originAddress
+          creatorAddress
+          blockNumber
+          removed
+          latestSolidifiedBlockNumber
+          logInfo
+          rawData
+          abi
+          eventSignature
+          eventSignatureFull
+          eventName
+          topicMap
+          dataMap
+        }
+      }`,
+    variables: { contractAddress }
+  }).subscribe({
+    next (data) {
+      handler(data)
+    }
+  });
+}
+
+const contractLogListener = (contractAddress = '', handler) => {
+  apolloClient.subscribe({
+    query: gql`
+      subscription contractLog($contractAddress: String){
+        eventsTopicContractLog(contractAddress: $contractAddress){
+          timeStamp
+          triggerName
+          uniqueId
+          transactionId
+          contractAddress
+          callerAddress
+          originAddress
+          creatorAddress
+          blockNumber
+          removed
+          latestSolidifiedBlockNumber
+          topicList
+          data
+        }
+      }`,
+    variables: { contractAddress }
+  }).subscribe({
+    next (data) {
+      handler(data)
+    }
+  });
+}
+
+module.exports = {
+  blockListener,
+  transactionListener,
+  contractEventListener,
+  contractLogListener
+}
